c54b3999ef960ccd29e7683687872068b4863fa3
piero <piero.dauber@gmail.com>
2019-08-16 21:40:07 +0200

    styling login. add scss color variables, functional utilities and buttons

--- src/modules/content/content.controller.js
+++ src/modules/content/content.controller.js
@@ -1,8 +1,41 @@
-function ContentController (){
+function ContentController (Api){
     var self = this;
-    
+    console.log('controller called')
+    // variables
+    self.listItems = [];
+
+    // functions
+    self.getInitData = getInitData;
+    self.showSingleItem = showSingleItem;
+
+    // life cycle
+    self.$onInit = function () {
+        getInitData();
+    }
+    self.$onChanges = function (obj) {
+    }
+    self.$onDestroy = function () {
+    }
+    self.$postLink = function () {
+    }
+    // declarations
+    function getInitData() {
+        Api.get('users?per_page=10').then(
+            function success(response) {
+                self.listItems = response.data;
+                console.log('response', response)
+            },
+            function error(error) {
+                console.log('error', error);
+            }
+        )
+    }
+
+    function showSingleItem(id) {
+        console.log('show single item: ', id);
+    }
 }
 
-ContentController.$inject = [];
+ContentController.$inject = ['Api'];
 
 export {ContentController}
