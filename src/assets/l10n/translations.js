68f782e6ec5a676792c6086bf2619b63c675a716
piero <piero.dauber@gmail.com>
2019-09-20 19:02:18 +0200

    delete branch

--- src/assets/l10n/translations.js
+++
@@ -1,5 +0,0 @@
-angular.module('webpack4.angularjs').run(['gettextCatalog', function (gettextCatalog) {
-/* jshint -W100 */
-    gettextCatalog.setStrings('it', {"Colors":"Colori","Get More Info":"Approfondisci","Log out":"Logout","Users":"Utenti"});
-/* jshint +W100 */
-}]);
--- src/modules/dashboard/user-dashboard/dashboard.component.js
+++
@@ -1,9 +0,0 @@
-import { UserDashboardController } from './dashboard.controller';
-
-const userDashboard = {
-    name: 'userDashboard',
-    template: require('./dashboard.view.html'),
-    controller: UserDashboardController,
-    controllerAs: 'uiCtrl',
-}
-export {userDashboard}
